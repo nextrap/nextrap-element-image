a81a2b23c68357ffbd5f7e03ab5a643b444f6d60
Marcel Frank <M.Frank@Decadis.de>
2025-04-15 00:18:23 +0200

    Update package and configuration to reflect new naming convention for the image component, changing references from 'nextrap-element-image' to 'nextrap-image'.

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'NextrapElementImage',
-      fileName: 'nextrap-element-image',
+      name: 'NextrapImage',
+      fileName: 'nextrap-image',
       formats: ['es', 'umd']
     },
     rollupOptions: {
